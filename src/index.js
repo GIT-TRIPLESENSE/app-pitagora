d056bf49e55109d2c6886b2fbf5fee4824e1feae
EliaFederico <fdrcelia@gmail.com>
2019-07-08 11:14:45 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.scss'
+import $ from 'jquery'
+import ScrollMagic from 'scrollmagic'
+import { TweenLite } from 'gsap'
+import 'slick-carousel/slick/slick.scss'
+
+window.$ = $ //jquery :)
+window.ScrollMagic = ScrollMagic
+
+$(document).ready(() => {
+    console.log('HELLO WORLD!')
+})
